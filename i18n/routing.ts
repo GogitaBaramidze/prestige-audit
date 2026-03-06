1734bb597256492954aa41e0b40b021893791c80
Lado <lado.asambadze1@gmail.com>
2026-03-06 17:34:06 +0400

    !

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -7,7 +7,7 @@ export type Locale = (typeof locales)[number];
 export const routing = defineRouting({
   locales: ["ka", "en"],
   defaultLocale: "ka",
-  localePrefix: "never",
+  localePrefix: "always",
   localeDetection: true,
 });
 

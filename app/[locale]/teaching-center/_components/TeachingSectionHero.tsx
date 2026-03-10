8c5fa3faaaf676e00ef4edf2aff4fbf94b121048
Lado <lado.asambadze1@gmail.com>
2026-03-10 17:38:52 +0400

    nv

--- app/[locale]/teaching-center/_components/TeachingSectionHero.tsx
+++ app/[locale]/teaching-center/_components/TeachingSectionHero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { GraduationCap, Info } from "lucide-react";
+import { Info } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 export default function TeachingCenterHeroSection() {
